aaef75f2c38ba73001fbeaf6d0a1e8048e0ccb5d
jennieemle <90578849+jennieemle@users.noreply.github.com>
2023-09-09 18:56:09 -0400

    Update 090623app.py

--- 090623app.py
+++ 090623app.py
@@ -9,22 +9,6 @@ import pandas as pd
 sview = pd.Series(view)
 sview
 
-st.title("Making a Button")
-result = st.button('click 버튼1')
-st.write(result)
-if result:
-    st.write('message : 버튼1이 클릭되었습니다.')
-
-st.button("Reset", type="primary")
-if st.button('Hi 버튼2'):
-    st.write('message : 버튼2 클릭하셨습니다.')
-else:
-    st.write('clear')
-
-result = st.button('버튼3')
-if result:
-    st.write('message : 버튼3이 클릭되었습니다.')
-
 st.title("Simple Button")
 result = st.button('simple_button')
 if result:
