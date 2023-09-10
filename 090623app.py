cc2526ff860e8294f5c6eadfdd44b975562cb2c9
jennieemle <90578849+jennieemle@users.noreply.github.com>
2023-09-09 23:39:57 -0400

    Update 090623app.py

--- 090623app.py
+++ 090623app.py
@@ -37,7 +37,7 @@ hide_streamlit_style = """
             #MainMenu {visibility: hidden;}
             header {visibility: hidden;}
             footer {visibility: hidden;}
-            manage-app-button {display: none;}
+            .styles_terminalButton_JBj5T {visibility: hidden;}
             </style>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
