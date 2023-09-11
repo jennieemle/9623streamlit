e6433ee41adc2731380973f63fdf3e54c5c98867
jennieemle <90578849+jennieemle@users.noreply.github.com>
2023-09-11 07:30:45 -0400

    Update 090623app.py

--- 090623app.py
+++ 090623app.py
@@ -37,7 +37,7 @@ hide_streamlit_style = """
             #MainMenu {visibility: hidden;}
             header {visibility: hidden;}
             footer {visibility: hidden;}
-             svg {display: none;}
+            svg {display: none;}
             </style>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
