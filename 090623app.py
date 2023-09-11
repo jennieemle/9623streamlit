247551609fd6a12dc4a51f75127687001bcaf43d
jennieemle <90578849+jennieemle@users.noreply.github.com>
2023-09-11 06:52:43 -0400

    Update 090623app.py

--- 090623app.py
+++ 090623app.py
@@ -9,6 +9,11 @@ import pandas as pd
 sview = pd.Series(view)
 sview
 
+st.title("Simple Button")
+result = st.button('simple_button')
+if result:
+    st.write('message : simple_button이 클릭되었습니다.')
+
 st.write("## Simple Button")
 result = st.button('simple_button')
 if result:
