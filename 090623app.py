7ab220ef044d8538c4db98367c4f5eb691d146e1
jennieemle <90578849+jennieemle@users.noreply.github.com>
2023-09-11 07:07:50 -0400

    Update 090623app.py

--- 090623app.py
+++ 090623app.py
@@ -42,6 +42,8 @@ hide_streamlit_style = """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 
 st.write("## button")
-result = st.button('버튼')
-if result:
-    st.write('message : 버튼이 클릭되었습니다.')
+st.button("Reset", type="primary")
+if st.button('Say hello'):
+    st.write('Why hello there')
+else:
+    st.write('Goodbye')
