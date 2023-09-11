e9ed23b866c026a60c5ed37903fc1e7cbfc88687
jennieemle <90578849+jennieemle@users.noreply.github.com>
2023-09-11 06:53:48 -0400

    Update 090623app.py

--- 090623app.py
+++ 090623app.py
@@ -14,11 +14,6 @@ result = st.button('simple_button')
 if result:
     st.write('message : simple_button이 클릭되었습니다.')
 
-st.write("## Simple Button")
-result = st.button('simple_button')
-if result:
-    st.write('message : simple_button이 클릭되었습니다.')
-
 st.write("## text input")
 title = st.text_input('종목코드')
 st.write('현재 종목코드 : ', title)
