b9f86cf12eae139fa81473213da78f2a0c807b23
jennieemle <90578849+jennieemle@users.noreply.github.com>
2023-09-12 13:51:26 -0400

    Update 090623app.py

--- 090623app.py
+++ 090623app.py
@@ -38,7 +38,7 @@ hide_streamlit_style = """
             header {visibility: hidden;}
             footer {visibility: hidden;}
             a.viewerBadge_container__r5tak styles_viewerBadge__CvC9N {visibility: hidden;}
-            div..viewerBadge_link__qRIco {visibility: hidden;}
+            div.viewerBadge_link__qRIco {visibility: hidden;}
             </style>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
